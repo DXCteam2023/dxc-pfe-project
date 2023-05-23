c80dde236b15630e589697f2d89f282466f2405a
Amjad EL MERABET <45132339+amjadelmerabet@users.noreply.github.com>
2023-05-23 16:32:44 +0100

    Tailwindcss fixed

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,5 @@
-import Header from '../component/header'
+import Header from "../components/header";
 
 export default function Home() {
-  return (
-    
-      <Header/>
-    
-  )
+    return <Header />;
 }
