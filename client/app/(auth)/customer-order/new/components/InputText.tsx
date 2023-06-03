9e8a9d302da1613591aea933ce426b8aa340afa6
HalaBouanani <bouananihala@gmail.com>
2023-06-03 21:11:23 +0100

    4th page

--- client/app/(auth)/customer-order/new/components/InputText.tsx
+++ client/app/(auth)/customer-order/new/components/InputText.tsx
@@ -5,6 +5,7 @@ type InputTextType = {
   title: string;
   required?: boolean;
   placeholder?: string;
+  value?: any;
 };
 
 export default function InputText({
@@ -12,6 +13,7 @@ export default function InputText({
   title,
   required = false,
   placeholder,
+  value,
 }: InputTextType) {
   return (
     <div className="sm:col-span-3 w-full">
@@ -28,6 +30,7 @@ export default function InputText({
           id={slug}
           placeholder={placeholder}
           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#5f249f] sm:text-sm sm:leading-6"
+          value={value}
         />
       </div>
     </div>
--- client/app/(auth)/customer-order/new/review-order/page.tsx
+++ client/app/(auth)/customer-order/new/review-order/page.tsx
@@ -1,5 +1,55 @@
 "use client";
 
+import ItemDetails from "./ItemDetails";
+
+const COLUMNS = [
+  { title: "Numbers" },
+  { title: "Product Offering" },
+  { title: "Product Specification" },
+  { title: "Ordered Quantity" },
+  { title: "Location" },
+];
+
+const DATA = Array(5)
+  .fill(0)
+  .map((_, i) => ({
+    number: "ORDL00099" + i,
+    productOffering: "Premium SD WAN OFFERING " + i,
+    productSpecification: "SD WAN SERVICE PACKAGE " + i,
+    orderedQuantity: 1,
+    location: "100 south charles street Batiment MD " + i,
+  }));
+
 export default function ReviewOrder() {
-  return <div className="review-order">ReviewOrder</div>;
+  return (
+    <div className="review-order w-full p-4">
+      <h4 className="font-extrabold">Order summary</h4>
+      <h6>List</h6>
+      <div className="flex gap-4">
+        <table className="table-auto w-full border-2">
+          <thead>
+            <tr className="border-2">
+              {COLUMNS.map((column) => (
+                <th>{column.title}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {DATA.map((item) => (
+              <tr className="border-2 text-center">
+                <td>{item.number}</td>
+                <td>{item.productOffering}</td>
+                <td>{item.productSpecification}</td>
+                <td>{item.orderedQuantity}</td>
+                <td>{item.location}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div>
+          <ItemDetails item={DATA[0]} />
+        </div>
+      </div>
+    </div>
+  );
 }
