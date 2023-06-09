8e1b1dda58f8d924523e9e3b09b75000ef0709f2
ibtissamHd <hdidouchibtissam@gmail.com>
2023-06-09 16:17:14 +0100

    prodord

--- client/app/(auth)/customer-order/all/product/page.tsx
+++ client/app/(auth)/customer-order/all/product/page.tsx
@@ -1,7 +1,22 @@
+import React from "react";
+import "./styles.css";
+import Header from "../../../dashboard/components/Header";
+import Footer from "../../../dashboard/components/Footer";
+import ProductOrders from "./components/ProductOrders";
+import Sidebar from "../../../dashboard/components/Sidebar";
+
 export default function ProductCustomerOrdersPage() {
-    return (
-        <div className="product-customer-orders">
-            <h1>Product Customer Orders Page</h1>
+  return (
+    <div className="product-customer-orders">
+      <div className="bg-gray-100 flex">
+        <Sidebar />
+        <div className="bg-white min-h-screen-100 w-5/6">
+          <Header />
+          <ProductOrders />
+
+          <Footer />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
