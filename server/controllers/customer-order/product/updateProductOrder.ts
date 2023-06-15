f106481d85d71145081b12b6b9ecb90558241425
Amjad EL MERABET <45132339+amjadelmerabet@users.noreply.github.com>
2023-06-15 22:38:38 +0100

    Removing next-auth for deployment

--- server/controllers/customer-order/product/updateProductOrder.ts
+++ server/controllers/customer-order/product/updateProductOrder.ts
@@ -1,3 +1,22 @@
 import { Request, Response } from "express";
+import { ObjectId } from "mongodb";
+import { ProductOrder } from "../../../models/productOrder";
 
-export default async function updateProductOrder(req: Request, res: Response) {}
+export default async function updateProductOrder(req: Request, res: Response) {
+  const { id } = req.params;
+  const mongodbId = new ObjectId(id);
+  const updates = req.body;
+  res.setHeader("Content-Type", "application/json");
+  try {
+    const productOrder = await ProductOrder.findByIdAndUpdate(
+      mongodbId,
+      updates,
+      { new: true }
+    );
+    res
+      .status(200)
+      .send(JSON.stringify({ message: "Product order updated", productOrder }));
+  } catch (e) {
+    res.status(500).send(JSON.stringify({ error: e }));
+  }
+}
