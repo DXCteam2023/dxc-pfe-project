f2d2a0f12f4dab026cf4438ec594665a5744976e
Amjad EL MERABET <45132339+amjadelmerabet@users.noreply.github.com>
2023-06-16 12:38:11 +0100

    NextAuth removed

--- server/controllers/customer-order/product/addProductOrder.ts
+++ server/controllers/customer-order/product/addProductOrder.ts
@@ -1,20 +1,4 @@
 import { Request, Response } from "express";
 import { ProductOrder } from "../../../models/productOrder";
 
-export default async function addProductOrder(req: Request, res: Response) {
-  try {
-    const payload = req.body;
-    const productOrder = await ProductOrder.create(payload);
-    res.setHeader("Content-Type", "application/json");
-    res.status(201).send(
-      JSON.stringify({
-        message: "New product order inserted",
-        productOrder: JSON.stringify(productOrder),
-      })
-    );
-  } catch (e) {
-    res.setHeader("Content-Type", "application/json");
-    res.status(500).send(JSON.stringify({ exeption: e }));
-    // console.log(JSON.stringify(po));
-  }
-}
+export default async function addProductOrder(req: Request, res: Response) {}
--- server/controllers/customer-order/product/updateProductOrder.ts
+++ server/controllers/customer-order/product/updateProductOrder.ts
@@ -2,21 +2,4 @@ import { Request, Response } from "express";
 import { ObjectId } from "mongodb";
 import { ProductOrder } from "../../../models/productOrder";
 
-export default async function updateProductOrder(req: Request, res: Response) {
-  const { id } = req.params;
-  const mongodbId = new ObjectId(id);
-  const updates = req.body;
-  res.setHeader("Content-Type", "application/json");
-  try {
-    const productOrder = await ProductOrder.findByIdAndUpdate(
-      mongodbId,
-      updates,
-      { new: true }
-    );
-    res
-      .status(200)
-      .send(JSON.stringify({ message: "Product order updated", productOrder }));
-  } catch (e) {
-    res.status(500).send(JSON.stringify({ error: e }));
-  }
-}
+export default async function updateProductOrder(req: Request, res: Response) {}
