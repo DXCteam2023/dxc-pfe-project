734bf447c96d089f6a5f36833c04540dd1ee266f
nouhailatahri <85485874+nouhailatahri@users.noreply.github.com>
2023-06-21 10:04:54 +0100

    this is account management code

---
+++ server/controllers/account/getAccount.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import  Account  from "../../models/account";
+
+export default async function getAccount(req: Request, res: Response) {
+
+    try {
+        const accounts = await Account.find();
+      
+        res.setHeader("Content-Type", "application/json");
+        res.setHeader("x-Total-Count", accounts.length);
+        res.status(200).send(JSON.stringify(accounts));
+      } catch (error) {
+        console.error("Error retrieving product orders:", error);
+        res.status(500).send("Internal Server Error");
+      }
+}
