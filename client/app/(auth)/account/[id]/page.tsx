77e729ad6abf60b746ed36a2bb7b60068416a301
Amjad EL MERABET <45132339+amjadelmerabet@users.noreply.github.com>
2023-06-23 15:16:57 +0100

    Formatting for deployment

--- client/app/(auth)/account/[id]/page.tsx
+++ client/app/(auth)/account/[id]/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+
 import Sidebar from "../../dashboard/components/Sidebar";
 import Header from "../../dashboard/components/header/Header";
+import Footer from "../../dashboard/components/Footer";
 
 // Importing utility functions
 import getAccount from "../utils/getAccount";
-import Footer from "../../dashboard/components/Footer";
 
 const SingleAcountPage = ({ params }: { params: { id: string } }) => {
   const [account, setAccount] = useState<any>();
