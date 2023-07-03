19a97c7122cc0cf928e891d831db2466ad9cc258
Amjad EL MERABET <45132339+amjadelmerabet@users.noreply.github.com>
2023-07-03 10:14:06 +0100

    Updates to the product offering publishing feature

--- server/controllers/product-offering/publishProductOffering.ts
+++ server/controllers/product-offering/publishProductOffering.ts
@@ -1,29 +1,41 @@
 import { Request, Response } from "express";
 import axios from "axios";
 
+export default async function publishProductOffering(
+  req: Request,
+  res: Response
+) {
+  const id = req.params.id;
+  try {
+    // Make the POST request to ServiceNow API
+    const url = `https://dev174830.service-now.com/api/sn_ind_tmt_orm/catalogmanagement/productoffering/${id}`;
+    const auth = {
+      username: "admin",
+      password: "rL4=I7iLPw%n",
+    };
+    const payload = {};
 
-export default async function publishProductOffering(req: Request, res: Response) {
-    try {
-        // Make the POST request to ServiceNow API
-        const url = "https://dev174830.service-now.com/api/sn_ind_tmt_orm/catalogmanagement/productoffering";
-        const auth = {
-          username: "admin",
-          password: "rL4=I7iLPw%n",
-        };
-        const payload = {};
-    
-        const response = await axios.post(url, payload, { auth });
-    
-        if (response.status === 200) {
-          console.log("Product offering published successfully in ServiceNow .");
-        } else {
-          console.error("Failed to publishing the Product offering in ServiceNow", response.data);
-        }
-    
-      res.status(201).send({ message: "Product Published successfully" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ message: "Internal server error - publishing product" });
+    const response = await axios.post(url, payload, {
+      auth,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status === 200) {
+      console.log("Product offering published successfully in ServiceNow.");
+    } else {
+      console.error(
+        "Failed to publishing the Product offering in ServiceNow",
+        response.data
+      );
     }
+
+    res.status(201).send({ message: "Product Published successfully" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send({ message: "Internal server error - publishing product" });
   }
-  
+}
