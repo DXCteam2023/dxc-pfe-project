09c8e0bd2f81074e11d4583bf3eeff5666ae4a8d
HalaBouanani <bouananihala@gmail.com>
2023-07-15 00:33:58 +0100

    add update product order to servicenow

--- server/controllers/customer-order/product/addProductOrderToServiceNow.ts
+++ server/controllers/customer-order/product/addProductOrderToServiceNow.ts
@@ -1,8 +1,9 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 import axios from "axios";
 import { ObjectId } from "mongodb";
-import DataModel from "../../../models/data";
 import qs from "qs";
+
+import DataModel from "../../../models/data";
 
 export default async function addProductOrderToServiceNow(
   req: Request,
@@ -169,5 +170,77 @@ export default async function addProductOrderToServiceNow(
     }
   } catch (error) {
     return { error };
+  }
+}
+const INSTANCE_URL = "https://dev174830.service-now.com"
+
+const COOKIE = "BIGipServerpool_dev174830=495998730.35134.0000; JSESSIONID=A2E08104C4CC46B511A35B4E0CCF4EC5; glide_session_store=8D9179DB97EFAD10720F7A200153AFC8; glide_user_route=glide.dcbc4af8666824ee4372a55062687d69"
+
+const oAuthData = qs.stringify({
+  grant_type: "password",
+  client_id: "7c31b95f18efad109790b6fa6d1bc926",
+  client_secret: "<jna9DEIp(",
+  username: "technical.user",
+  password:
+    "#unD&gU^0O)?qI*+gPRqw98wN#GsEi30@w54;IQ](W.xf?VVG@@y0SixZTt&R,aaz+@iJBRt{Bx+?PNXYq-a)1f1:dwmkV1uXu@h",
+});
+
+const oAuthTokenConfig = {
+  method: "post",
+  maxBodyLength: Infinity,
+  url: `${INSTANCE_URL}/oauth_token.do`,
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Cookie: COOKIE,
+  },
+  data: oAuthData,
+};
+
+const getUpdateProductOrderToServiceNowConfig = (id, token, payload) => {
+  return {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: `${INSTANCE_URL}/api/sn_ind_tmt_orm/order/productOrder/${id}`,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+      Cookie: COOKIE,
+    },
+    data: payload,
+  }
+}
+
+export const updateProductOrderToServiceNow = async (
+  req: Request,
+  res: Response) => {
+  try {
+    const id = req.body.id;
+    const payload = req.body.data;
+
+    // get access_token from mongodb
+    const data = await DataModel.findOne({});
+    const access_token = data.access_token;
+
+    try {
+      // first, we try with the access_token from mongodb
+      const response = await axios.request(getUpdateProductOrderToServiceNowConfig(id, access_token, payload))
+      
+      // if no error catched, we return success and data
+      return res.status(200).json({ message: "success", data: response.data })
+      
+    } catch (error) {
+      // if error catched, we generate new access_token
+      const response2 = await axios.request(oAuthTokenConfig)
+      const access_token2 = response2.data
+      
+      // then we try again with the new access_token
+      const response3 = await axios.request(getUpdateProductOrderToServiceNowConfig(id, access_token2, payload))
+      
+      // if no error catched, we return success and data
+      return res.status(200).json({ message: "success", data: response3.data })
+    }
+  } catch (error) {
+    console.error('updateProductOrderToServiceNow', error)
+    return res.status(500).json({ message: "an error occured" })
   }
 }
